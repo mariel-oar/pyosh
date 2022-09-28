51f7f6b84eabfca896e2a63d78e1685a83ae43a3
KlausGPaul <20340525+KlausGPaul@users.noreply.github.com>
2022-09-28 16:46:35 +0000

    update to include source code comments

--- pyosh/pyosh.py
+++ pyosh/pyosh.py
@@ -1,14 +1,23 @@
 class PYOSH():
     """pyosh main class
+
+    Parameters
+    ----------
+    something : str, optional
+      Whatever
     """
-    def __init__(self):
+    def __init__(self,something=""):
+        """Init
+        """
         pass
 
-    def doobee(self,blah="empty"):
+    def doobee(self,bloo,blah="empty"):
         """module doobee, really does nothing fancy
 
         Parameters
         ----------
+        bloo : int
+          Some sort of input
         token : str, optional
           Access token to authenticate to the API
 
@@ -16,6 +25,11 @@ class PYOSH():
         -------
         None
           Nothing useful
+        """
+        pass
+
+    def something_else(self):
+        """Not really used.
         """
         pass
     
