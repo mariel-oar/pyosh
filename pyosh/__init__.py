dc79215d2841a15ae34ba1756c1a452fbf9f1f57
KlausGPaul <20340525+KlausGPaul@users.noreply.github.com>
2022-09-29 10:06:45 +0000

    updates

--- pyosh/__init__.py
+++ pyosh/__init__.py
@@ -1,3 +1,4 @@
+"""pyosh Package for accessing the https://opensupplyhub.org API using python"""
 from .pyosh import OSH_API
-"""pyosh Package for accessing the https://opensupplyhub.org API using python
+"""OSH_API class for accessing the https://opensupplyhub.org API using python
 """
