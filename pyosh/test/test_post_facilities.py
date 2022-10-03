a76e5e168444a8d06b5323532363882383fdc0b2
KlausGPaul <20340525+KlausGPaul@users.noreply.github.com>
2022-10-03 10:36:54 +0000

    updates to create facility

--- pyosh/test/test_post_facilities.py
+++ pyosh/test/test_post_facilities.py
@@ -10,6 +10,28 @@ def vcr_config():
 osh_api = pyosh.OSH_API()
 
 @pytest.mark.vcr()
-def test_post_facilities():
+def test_post_facilities_match():
     global osh_api
-    result = osh_api.post_facilities()
+    result = osh_api.post_facilities(
+        name="Rudolf-Diesel Gymnasium",
+        country="DE",
+        address="Peterhofstraße 9, 86163 Augsburg"
+    )
+
+@pytest.mark.vcr()
+def test_post_facilities_potential_match():
+    global osh_api
+    result = osh_api.post_facilities(
+        name="Birkenau Grundschule",
+        country="DE",
+        address="Soldnerstraße 35, 86167 Augsburg"
+    )
+
+@pytest.mark.vcr()
+def test_post_facilities_new():
+    global osh_api
+    result = osh_api.post_facilities(
+        name="Deedeldee",
+        country="DE",
+        address="Linke Brandstr. 21, 86165 Augsburg"
+    )
